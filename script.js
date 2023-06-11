d872fe321b8e262f427cd972a964b8ce97fba13b
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-11 21:39:56 +0300

    Added buttons to index.html, styling to style.css and onclick commands to script.js

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function changeStyle(){
 }
 changeStyle();
 document.querySelector("body").appendChild(marquee);
+
+document.getElementById("button1").addEventListener("click", displayAboutMe());
+document.getElementById("button2").addEventListener("click", displayExperience());
+document.getElementById("button3").addEventListener("click", displayContacts());
+
+function displayAboutMe(){
+    document.getElementById("para").innerHTML = "Brenda Chebet Bii";
+   /* let p = document.createElement("p");
+    let text;
+    text.textContent = "Brenda Chebet Bii";
+    p.appendChild(text);
+    document.getElementById("details").appendChild("p");
+    return details;*/
+
+};
+
+function displayExperience(){
+    document.getElementById("para").innerHTML = "I work hard.";
+    /*let text;
+    text.textContent = "Brenda Chebet Bii";
+    document.getElementById("para") = text;
+    return para;*/
+};
+
+function displayContacts(){
+    document.getElementById("para").innerHTML = "bii.brenda.bcb@gmail.com";
+};
